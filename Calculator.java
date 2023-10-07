76be223c35d53dc854975f455161349ffb51da27
ToXer04 <simonefarina2004@gmail.com>
2023-10-07 23:37:36 +0200

    Added the main method of the Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,48 +1,55 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        String[] arr = {};
+        double result = 0;
+        boolean bool = true;
         Scanner in = new Scanner(System.in);
-        System.out.println("Type an operation (ex: 1 + 2 =)");
-        String operation = in.nextLine();
-        String[] arr = operation.split(" ");
-        System.out.println(Arrays.toString(arr));
-        label:
-        for (int i = 0; i<arr.length; i++) {
-            try {
-                Double.parseDouble(arr[i]);
-            } catch (NumberFormatException nfe) {
-                switch (arr[i]) {
-                    case "+":
-                        double sum = sum(Double.parseDouble(arr[i - 1]), Double.parseDouble(arr[i + 1]));
-                        arr[i + 1] = String.valueOf(sum);
-                        System.out.println(Arrays.toString(arr));
-                        break;
-                    case "-":
-                        double sub = sub(Double.parseDouble(arr[i - 1]), Double.parseDouble(arr[i + 1]));
-                        arr[i + 1] = String.valueOf(sub);
-                        break;
-                    case "*":
-                        double mul = mul(Double.parseDouble(arr[i - 1]), Double.parseDouble(arr[i + 1]));
-                        arr[i + 1] = String.valueOf(mul);
-                        break;
-                    case "/":
-                        double div = div(Double.parseDouble(arr[i - 1]), Double.parseDouble(arr[i + 1]));
-                        arr[i + 1] = String.valueOf(div);
-                        break;
-                    case "^":
-                        double pow = pow(Double.parseDouble(arr[i - 1]), Double.parseDouble(arr[i + 1]));
-                        arr[i + 1] = String.valueOf(pow);
-                        break;
-                    case "=":
-                        System.out.print(arr[i - 1]);
-                        break label;
-                    default:
-                        System.out.println("Invalid character: " + arr[i]);
-                        break;
+        while (bool) {
+            System.out.println("Type a list of numbers (ex: 1, 2, 3.4, 5)");
+            String numbers = in.nextLine();
+            arr = numbers.split(", ");
+            for (String x : arr) {
+                try {
+                    if (Double.parseDouble(x) % 2 == 0) {
+                        System.out.println("The number " + x + " is even");
+                    } else if (Double.parseDouble(x) % 2 != 0) {
+                        System.out.println("The number " + x + " is odd");
+                    }
+                    bool = false;
+                } catch (NumberFormatException nfe) {
+                    System.out.println(x + " is a not valid a number");
+                    bool = true;
+                    break;
                 }
             }
         }
-        System.out.println(arr[arr.length-1]);
+        System.out.println("Type an operator ( + | - | * | / | ^ )");
+        String operator = in.nextLine();
+        if(arr.length == 0) {
+            System.out.println("To use the calculator you have to give as input at least a number");
+        } else {
+            switch (operator) {
+                case "+":
+                    result = sum(arr, result);
+                    break;
+                case "-":
+                    result = sub(arr, result);
+                    break;
+                case "*":
+                    result = mul(arr, result);
+                    break;
+                case "/":
+                    result = div(arr, result);
+                    break;
+                case "^":
+                    result = pow(arr, result);
+                    break;
+                default:
+            }
+            System.out.println(result);
+        }
     }
